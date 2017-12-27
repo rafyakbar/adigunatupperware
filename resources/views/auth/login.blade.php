cc3f5ff83f6c9bed1b0eaf8222e0cf025be5d6a5
Rafy Aulia Akbar <rafy683@gmail.com>
2017-12-27 23:13:52 +0700

    ui login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,70 +7,54 @@
     <title>Adiguna</title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
     <meta name="viewport" content="width=device-width"/>
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('css/material-dashboard.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('css/material-dashboard.css') }}" rel="stylesheet"/>
     <link href="{{ asset('css/dataTables.bootstrap.css') }}" rel="stylesheet"/>
     <link href="{{ asset('css/dataTables.responsive.css') }}" rel="stylesheet"/>
     <link href="{{ asset('css/font-awesome.min.css') }}" rel="stylesheet"/>
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet'
+          type='text/css'>
 </head>
 <body>
-<div class="container">
-    <div class="col-lg-4 col-md-3 col-sm-2"></div>
-    <div class="col-lg-4 col-md-6 col-sm-8">
-        <div class="logo">
-            <img src="assets/img/t.png" alt="Logo">
-        </div>
-        <div class="row loginbox">
-            <div class="col-lg-12">
-                <span class="singtext">Sign in </span>
+<div class="row" style="margin-top: 7%">
+    <div class="col-md-5" style="margin: 0 auto; float: none">
+        <div class="card">
+            <div class="card-header" data-background-color="purple">
+                <h4 class="title">Login</h4>
             </div>
-            <div class="col-lg-12 col-md-12 col-sm-12">
+            <div class="card-content" style="padding-left: 10%;padding-right: 8%">
                 <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                     {{ csrf_field() }}
-                    <br>
                     <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                        <label for="email" class="col-md-3 control-label">E-Mail</label>
-                        <div class="col-md-9">
-                            <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}"
-                                   required
-                                   autofocus>
-                            @if ($errors->has('email'))
-                                <span class="help-block">
-                            <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
+                        <label>E-Mail</label>
+                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}"
+                               required autofocus style="padding-left: 1%">
+                        @if ($errors->has('email'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
                     </div>
                     <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                        <label for="password" class="col-md-3 control-label">Password</label>
-                        <div class="col-md-9">
-                            <input id="password" type="password" class="form-control" name="password" required>
-                            @if ($errors->has('password'))
-                                <span class="help-block">
-                            <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                            @endif
-                        </div>
+                        <label for="password">Password</label>
+                        <input id="password" type="password" class="form-control" name="password" required style="padding: 1%">
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
                     </div>
-                    <br>
-                    <div class="form-check">
-                        <label class="col-md-6 form-check-label">
-                            <input class="form-check-input" type="checkbox"
-                                   name="remember" {{ old('remember') ? 'checked' : '' }}>
-                            Remember me
-                        </label>
+                    <div class="form-group">
+                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                        Remember me
                     </div>
-                    <button href="#" type="submit" class="btn  submitButton">Submit</button>
+                    <div class="form-group">
+                        <button href="#" type="submit" class="btn btn-success">Submit</button>
+                    </div>
                 </form>
             </div>
         </div>
-        <br>
-        <footer class="footer">
-            <p>©2016 - {{ \Carbon\Carbon::today()->year }} Adiguna All rights reserved </p>
-        </footer>
     </div>
-    <div class="col-lg-4 col-md-3 col-sm-2"></div>
 </div>
 </body>
 <script src="{{ asset('js/jquery-3.2.1.min.js') }}" type="text/javascript"></script>
